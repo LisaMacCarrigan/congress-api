9087a61cd801e4651ecb8cae562cef0ac57bb6b0
Lisa MacCarrigan <Lisa.MacCarrigan@gmail.com>
2016-10-31 14:49:15 -0400

    add env to store ApiKey, update gitignore to include env, update results route for ApiKe

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,8 +1,11 @@
+import config from '../config/environment';
 import Ember from 'ember';
+
 
 export default Ember.Route.extend({
   model: function(params) {
-    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=cd70edbb9f004458ba61e71e6c90e152&zip=' + params.zip;
+    var key = config.myApiKey;
+    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=' +key+ '&zip=' + params.zip;
     return Ember.$.getJSON(url).then(function(responseJSON) {
       return responseJSON.results;
     });
